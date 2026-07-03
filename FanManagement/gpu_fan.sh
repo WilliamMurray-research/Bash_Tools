4a52bedb22034bf7611da65cefc5edaa983165b3
William Murray <murray.global.risk@gmail.com>
2026-07-03 22:00:16 +0930

    Create gpu_fan.sh

---
+++ FanManagement/gpu_fan.sh
@@ -0,0 +1,14 @@
+[Unit]
+Description=A2 GPU Fan Control Persistence
+After=nvidia-persistenced.service multi-user.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/gpu_fan.sh
+Restart=always
+RestartSec=5
+OOMScoreAdjust=-1000
+
+[Install]
+WantedBy=multi-user.target
